28c7b635a3d8e85decf75e95615ace9961f9de8a
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2020-03-18 15:31:26 -0300

    Feature: start code in main.py file and ignore cache

---
+++ main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+if __name__ == '__main__':
+
+    # Builtins
+    import json
+
+    # My modules
+    from sample.process import start
+
+    data = start("josephine-langs-205th-birthday")
+
+    with open('./sample/data-google-doodles.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f, indent=4)
